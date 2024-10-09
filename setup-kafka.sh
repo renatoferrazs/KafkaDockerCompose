5da4d16b813fe13006021808824dcfaffbdfa39b
renatoferrazs <44041422+renatoferrazs@users.noreply.github.com>
2024-10-09 09:21:07 -0300

    Update setup-kafka.sh

--- setup-kafka.sh
+++ setup-kafka.sh
@@ -2,66 +2,59 @@
 
 # Atualiza a lista de pacotes
 echo "Atualizando lista de pacotes..."
-sudo apt-get update
+sudo apt-get update -qq
 
 # Instala pré-requisitos
 echo "Instalando pré-requisitos..."
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get install -y -qq ca-certificates curl gnupg lsb-release
 
 # Adiciona a chave GPG oficial do Docker
 echo "Adicionando chave GPG do Docker..."
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Adiciona o repositório do Docker
 echo "Adicionando repositório Docker..."
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Atualiza a lista de pacotes novamente
-sudo apt-get update
+# Atualiza a lista de pacotes novamente após adicionar o repositório Docker
+sudo apt-get update -qq
 
 # Instala Docker e Docker Compose
 echo "Instalando Docker e Docker Compose..."
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-compose
 
-# Verifica se a instalação foi bem-sucedida
-docker --version
-docker compose version
-
-# Inicia e habilita o serviço Docker
-echo "Iniciando e habilitando o Docker..."
-sudo systemctl start docker
-sudo systemctl enable docker
-
-# Permite que o usuário atual use o Docker sem sudo
+# Adiciona o usuário ao grupo Docker para evitar necessidade de sudo
 echo "Configurando permissões para o Docker..."
 sudo usermod -aG docker $USER
 
-# Cria o arquivo docker-compose.yml com as configurações do Kafka e Kafka UI
-echo "Criando arquivo docker-compose.yml..."
+echo "Para aplicar as mudanças, faça logout e login novamente."
+
 
 cat <<EOF > docker-compose.yml
-services:
-  zookeeper:
-    image: wurstmeister/zookeeper:3.4.6
-    ports:
-      - "2181:2181"
 
+services:
   kafka:
-    image: wurstmeister/kafka:latest
+    image: bitnami/kafka:latest
     ports:
       - "9092:9092"
       - "9093:9093"
     environment:
-      KAFKA_ADVERTISED_LISTENERS: INSIDE://kafka:9092,OUTSIDE://<host-ip>:9093
+      KAFKA_ADVERTISED_LISTENERS: INSIDE://kafka:9092,OUTSIDE://localhost:9093
       KAFKA_LISTENER_SECURITY_PROTOCOL_MAP: INSIDE:PLAINTEXT,OUTSIDE:PLAINTEXT
       KAFKA_LISTENERS: INSIDE://0.0.0.0:9092,OUTSIDE://0.0.0.0:9093
-      KAFKA_ZOOKEEPER_CONNECT: zookeeper:2181
+      KAFKA_ZOOKEEPER_HOSTS: zookeeper:2181
       KAFKA_INTER_BROKER_LISTENER_NAME: INSIDE
     depends_on:
       - zookeeper
+      
+  zookeeper:
+    image: bitnami/zookeeper:latest
+    ports:
+      - 2181:2181
+    environment:
+      - ALLOW_ANONYMOUS_LOGIN=yes
 
   kafka-ui:
     image: provectuslabs/kafka-ui:latest
@@ -73,18 +66,20 @@ services:
       KAFKA_CLUSTERS_0_ZOOKEEPER: zookeeper:2181
       KAFKA_CLUSTERS_0_PROPERTIES_SECURITY_PROTOCOL: PLAINTEXT
       
-      # Configuração do segundo cluster
+      # Configuração do segundo cluster caso tenha
       # KAFKA_CLUSTERS_1_NAME: segundo-cluster
       # KAFKA_CLUSTERS_1_BOOTSTRAPSERVERS: <ip-do-segundo-cluster>:9092
       # KAFKA_CLUSTERS_1_ZOOKEEPER: <ip-do-segundo-cluster>:2181
       # KAFKA_CLUSTERS_1_PROPERTIES_SECURITY_PROTOCOL: PLAINTEXT
     depends_on:
       - kafka
+
 EOF
 
-
-# Subindo os containers com Docker Compose
+# Inicia os containers usando Docker Compose
 echo "Rodando Docker Compose..."
-docker compose up -d
+if ! docker-compose up -d; then
+    echo "Erro ao iniciar os containers. Verifique os logs para mais detalhes."
+fi
 
 echo "Instalação e execução concluídas! Acesse o Kafka UI via http://localhost:8080"
